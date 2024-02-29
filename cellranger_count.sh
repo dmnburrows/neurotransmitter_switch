a2d6850a170a3a608b2b41276256cac490b5a781
dmnburrows <dominicburrows@hotmail.co.uk>
2024-02-29 02:50:20 +0000

    starting processing combined pilot

--- cellranger_count.sh
+++ cellranger_count.sh
@@ -1,7 +1,6 @@
 #/!/bin/bash
 
-cellranger count --id=cellranger  --transcriptome=/cndd/Public_Datasets/references/mm10_cellranger_H2BmCherry/mm10_H2B-mCherry --fastqs=/cndd3/dburrows/DATA/neurotransmitter_switch/raw/pilot/combined  --sample=combined-pilot --chemistry=SC3Pv3 --include-introns=true --localcores=10
+cellranger count --id=cellranger  --transcriptome=/cndd3/dburrows/DATA/annotations/genome/grcm38.p3/cellranger/mm10_all --fastqs=/cndd3/dburrows/DATA/neurotransmitter_switch/raw/pilot/combined  --sample=combined-pilot --chemistry=SC3Pv3 --include-introns=true --localcores=10
 
-cellranger mkref --genome=mm10_all --fasta=../GRCm38.p3.genome_all.fa --genes=../gencode.vM3.chr_patch_hapl_scaff.annotation.gtf
 
 
