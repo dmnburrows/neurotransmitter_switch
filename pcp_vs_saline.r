f3ae6682266d14a40ed599ea8d22a2069c06f8d3
dmnburrows <dominicburrows@hotmail.co.uk>
2024-08-07 18:22:47 -0700

    mast

--- pcp_vs_saline.r
+++ pcp_vs_saline.r
@@ -3,18 +3,29 @@ library(tidyverse)
 
 setwd("/cndd3/dburrows/DATA/neurotransmitter_switch/analysis/deseq/")
 
-coldata <- read.csv("design.csv", row.names=1)
-cnts <- read.csv("cnts.csv", header=TRUE, check.names=FALSE, row.names=1)
+# Get list of all files that start with 'cnts'
+cnts_files <- list.files(pattern = "^cnts.*\\.csv$")
 
-dds <-DESeqDataSetFromMatrix(countData=cnts, 
-                       colData=coldata, 
-                       design=~condition) 
+# Read the coldata file once (assuming it's the same for all count files)
+coldata <- read.csv("design.csv", row.names = 1)
 
-
-
-dds <- DESeq(dds)
-res <- results(dds, contrast = c("condition", "PCP", "Sal"), alpha = 0.1)
-write.csv(as.data.frame(res), 
-          file="outs.csv")
-
-
+# Loop over each count file
+for (cnts_file in cnts_files) {
+  # Read the count data
+  cnts <- read.csv(cnts_file, header = TRUE, check.names = FALSE, row.names = 1)
+  
+  # Create DESeq2 dataset
+  dds <- DESeqDataSetFromMatrix(countData = cnts, 
+                                colData = coldata, 
+                                design = ~condition)
+  
+  # Run DESeq2
+  dds <- DESeq(dds)
+  res <- results(dds, contrast = c("condition", "PCP", "Sal"), alpha = 0.1)
+  
+  # Define the output filename
+  output_file <- paste0("outs_", sub("\\.csv$", "", cnts_file), ".csv")
+  
+  # Save the results
+  write.csv(as.data.frame(res), file = output_file)
+}
